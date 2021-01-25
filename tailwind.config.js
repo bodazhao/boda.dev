5003297835bfac6c16ace059175f83e6a895e860
Boda Zhao <bodazhao@gmail.com>
2021-01-25 23:42:01 +0000

    chore: upgrade deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,6 @@ module.exports = {
   theme: {},
   variants: {},
   plugins: [require("@tailwindcss/typography")],
-  future: {
-    removeDeprecatedGapUtilities: true,
-    // https://tailwindcss.com/docs/upcoming-changes#purge-layers-by-default
-    purgeLayersByDefault: true,
-  },
   purge: {
     // https://tailwindcss.com/docs/controlling-file-size
     enabled: process.env.NODE_ENV === "production",
